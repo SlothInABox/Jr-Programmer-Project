1bda0b5e7b58b25a2c9c645abbd21a265d36932e
James Goodman <43926484+SlothInABox@users.noreply.github.com>
2021-07-05 00:03:07 +0100

    Added data persistence between sessions
    - Data is now saved to JSON file
    - Data saved when session ends and loaded when session starts
    - Added testing buttons

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -24,6 +24,7 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.instance.teamColor); // Pre-select saved color (if one exists)
     }
 
     public void StartNew()
@@ -33,10 +34,23 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
+        MainManager.instance.SaveColor();
+
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
         Application.Quit();
 #endif
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.instance.teamColor);
     }
 }
